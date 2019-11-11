6162d20465dde18505ec541a46f13bd7d2081566
Алсу Сабирова <Rouz1603@gmail.com>
2019-11-11 16:30:33 +0300

    Third commit

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QPen, QColor
 from random import randint
 from PyQt5 import uic
 
 
-class MyWidjet(QMainWindow):
+class MyWidjet(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.pushButton.clicked.connect(self.run)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(200, 200, 500, 500)
+        self.setWindowTitle('Окружности')
+
+        self.btn = QPushButton('клик', self)
+        self.btn.resize(100, 30)
+        self.btn.move(200, 450)
+
+        self.btn.clicked.connect(self.run)
         self.flag = False
 
     def run(self):
@@ -25,9 +34,9 @@ class MyWidjet(QMainWindow):
             self.qp.end()
 
     def draw(self, qp):
-        pen = QPen(Qt.yellow, 3)
+        pen = QPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255)), 6)
         self.qp.setPen(pen)
-        x = randint(100, 600)
+        x = randint(100, 400)
         y = randint(100, 400)
         r = randint(0, 100)
         self.qp.drawEllipse(x, y, r, r)
